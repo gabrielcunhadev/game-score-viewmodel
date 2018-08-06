86e3faa2bd91e3ee28f99839aca2d279059182fc
Gabriel Cunha <gabrielcunhanet@gmail.com>
2018-08-05 22:28:18 -0300

    Create unit test for viewModel ScoreViewModel.

--- app/src/test/java/com/gabrielcunha/gamescore/ExampleUnitTest.kt
+++ app/src/test/java/com/gabrielcunha/gamescore/ExampleUnitTest.kt
@@ -1,17 +1,40 @@
 package com.gabrielcunha.gamescore
 
+import android.arch.lifecycle.ViewModelProviders
+import com.gabrielcunha.gamescore.viewmodel.ScoreViewModel
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
+@RunWith(RobolectricTestRunner::class)
 class ExampleUnitTest {
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun changeScoreTest() {
+        val vm = getViewModel()
+        vm.updateTimeA()
+        assertEquals(1, vm._scoreTeamA.value)
+
+        vm.updateTimeB()
+        assertEquals(1, vm._scoreTeamB.value)
+
     }
+
+    @Test
+    fun resetTest() {
+        val vm = getViewModel()
+        vm.reset()
+        assertEquals(0, vm._scoreTeamA.value)
+        assertEquals(0, vm._scoreTeamB.value)
+    }
+
+
+    private fun getViewModel() : ScoreViewModel {
+        val activity = Robolectric.setupActivity(MainActivity::class.java)
+        return ViewModelProviders.of(activity).get(ScoreViewModel::class.java)
+    }
+
 }
